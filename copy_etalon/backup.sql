a4dd643994c1a770e8ea33f11fe1a424efc6e193
AlexeyZzl <52735416+AlexeyZzl@users.noreply.github.com>
2019-07-11 07:51:56 +0000

    Initial commit

---
+++ copy_etalon/backup.sql
@@ -0,0 +1,10 @@
+﻿DECLARE @mydb nvarchar(50);
+-- Ввести имя базы данных
+set @mydb = '$(backupdb)';
+
+DECLARE @back_file nvarchar(300);
+-- Ввести расположение файла бэкапа
+set @back_file = '$(bakfile)';
+
+BACKUP DATABASE @mydb TO  DISK = @back_file WITH NOFORMAT, NOINIT,  NAME = N'Full backup', SKIP, NOREWIND, NOUNLOAD, COMPRESSION,  STATS = 10
+GO
